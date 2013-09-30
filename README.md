e8907fcfd0087cf86ca8f4dfbf8d6a08ddb8a808
zhangjingqiang <zhangjingqiang@gmail.com>
2013-09-30 13:46:02 +0900

    Init

---
+++ README.md
@@ -0,0 +1,12 @@
+Redis + Ruby Shortlink
+======================
+
+# Introduction
+
+A shortlink app by [sinatra](http://www.sinatrarb.com/) and [redis](http://redis.io)
+
+Hosted by [appfog](redis-ruby-shortlink.ap01.aws.af.cm)
+
+## Reference
+
+[How to Build a Shortlink App with Ruby and Redis](http://net.tutsplus.com/tutorials/ruby/how-to-build-a-shortlink-app-with-ruby-and-redis/)
---
+++ app.rb
@@ -0,0 +1,32 @@
+require 'sinatra'
+require 'redis'
+set :protection, except: :ip_spoofing
+
+redis = Redis.new
+
+helpers do
+  include Rack::Utils
+  alias_method :h, :escape_html
+
+  def random_string(length)
+    rand(36**length).to_s(36)
+  end
+end
+
+get '/' do
+  erb :index
+end
+
+post '/' do
+  if params[:url] and not params[:url].empty?
+    @shortcode = random_string 5
+    redis.setnx "links:#{@shortcode}", params[:url]
+  end
+  erb :index
+end
+
+get '/:shortcode' do
+  @url = redis.get "links:#{params[:shortcode]}"
+  redirect @url || '/'
+end
+
